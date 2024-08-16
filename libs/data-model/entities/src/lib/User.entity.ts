c36a3357287812214ff618890b11de3fc7fc586b
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-16 14:09:32 -0700

    Add the user entity

---
+++ libs/data-model/entities/src/lib/User.entity.ts
@@ -0,0 +1,9 @@
+/* The User class in TypeScript defines properties for a user's name, username, password, creation
+date, and update date. */
+export class User {
+	name = '';
+	username = '';
+	password = '';
+	createAt?: Date;
+	updateAt?: Date;
+}
