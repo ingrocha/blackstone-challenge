c5c430dccd5f94acd789208f170e0353d764c6d2
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-16 11:24:16 -0700

    Add a new NodeJs project

---
+++ app/backend/src/main.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+// load the environment variables from the .env file
+dotenv.config({
+  path: '.env',
+});
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
