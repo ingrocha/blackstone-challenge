5234e988e4c83f37d427a66fe5b51b2ed0965011
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-19 16:59:39 -0700

    create mapper to remove unnecessary data and covert to entity

---
+++ app/backend/src/api/note/mapper/note.mapper.ts
@@ -0,0 +1,14 @@
+import { Note } from '@blackstone-challenge/data-model/entities';
+import { NoteOuputDto } from '../dto/note.dto';
+
+export const noteMapper = (note: NoteOuputDto): Note => {
+	return {
+		id: note._id.toString(),
+		title: note.title,
+		content: note.content,
+		author: note.author,
+		sharedUsers: note.sharedUsers,
+		createdAt: note.createdAt,
+		updatedAt: note.updatedAt,
+	};
+};
