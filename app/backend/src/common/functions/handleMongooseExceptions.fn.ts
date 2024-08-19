ce00ea26391c64f1e5f2def39733f5b299e0906a
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-19 16:54:33 -0700

    Added duplicate user validation in handleMongooseExceptions for shared notes

--- app/backend/src/common/functions/handleMongooseExceptions.fn.ts
+++ app/backend/src/common/functions/handleMongooseExceptions.fn.ts
@@ -2,9 +2,15 @@ import createHttpError from 'http-errors';
 
 export const handleMongooseExceptions = (error, model: string) => {
 	if (error.code === 11000) {
-		throw new createHttpError.BadRequest(
-			`${model} exists in db ${JSON.stringify(error.keyValue)}`
-		);
+		const keyValue = JSON.stringify(error.keyValue);
+		if (keyValue.includes('sharedUsers.username'))
+			throw new createHttpError.BadRequest(
+				`Duplicate user detected in sharedUsers. Each user can only be added once per note.`
+			);
+		else
+			throw new createHttpError.BadRequest(
+				`${model} exists in db ${JSON.stringify(error.keyValue)}`
+			);
 	}
 	console.log(error);
 	throw new createHttpError.InternalServerError(
