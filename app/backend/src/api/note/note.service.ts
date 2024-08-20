67e8f00a652a7c55c6abbe51a4bd0a8ada668707
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-19 17:02:01 -0700

    create service for database interaction

---
+++ app/backend/src/api/note/note.service.ts
@@ -0,0 +1,90 @@
+import createHttpError from 'http-errors';
+import NoteModel from '../../db/models/note.model';
+import { NoteInputDto, NoteOuputDto } from './dto/note.dto';
+import { handleMongooseExceptions } from '../../common/functions/handleMongooseExceptions.fn';
+
+export const findById = async (id: string): Promise<NoteOuputDto> => {
+	const note = await NoteModel.findById<NoteOuputDto>(id);
+
+	if (!note)
+		throw new createHttpError.NotFound(`The note id ${id} does not exist`);
+
+	return note;
+};
+
+export const findAll = async (
+	username: string,
+	searchTerm?: string
+): Promise<NoteOuputDto[]> => {
+	const queryParams = {};
+	queryParams['author'] = username;
+	if (searchTerm)
+		queryParams['$or'] = [
+			{ title: { $regex: '.*' + searchTerm + '.*' } },
+			{ content: { $regex: '.*' + searchTerm + '.*' } },
+		];
+	const notes = await NoteModel.find<NoteOuputDto>(queryParams).sort({
+		createdAt: -1,
+	});
+	return notes;
+};
+
+export const findSharedNotes = async (
+	username: string,
+	searchTerm?: string
+): Promise<NoteOuputDto[]> => {
+	const queryParams = {};
+	queryParams['author'] = username;
+	if (searchTerm)
+		queryParams['$or'] = [
+			{ title: { $regex: '.*' + searchTerm + '.*' } },
+			{ content: { $regex: '.*' + searchTerm + '.*' } },
+		];
+	const sharedNotes: NoteOuputDto[] = await NoteModel.find({
+		'sharedUsers.username': username,
+		$or: [{ title: `/${searchTerm}/` }, { content: `/${searchTerm}/` }],
+	});
+
+	return sharedNotes;
+};
+
+export const create = async (
+	noteInputDto: NoteInputDto
+): Promise<NoteOuputDto> => {
+	try {
+		const note = new NoteModel(noteInputDto);
+		return await note.save();
+	} catch (error) {
+		handleMongooseExceptions(error, 'Note');
+	}
+};
+
+export const update = async (
+	id: string,
+	noteInputDto: NoteInputDto
+): Promise<NoteOuputDto> => {
+	try {
+		const note = await findById(id);
+
+		await note.updateOne(noteInputDto);
+
+		return { ...note.toJSON(), ...noteInputDto };
+	} catch (error) {
+		handleMongooseExceptions(error, 'Note');
+	}
+};
+
+export async function remove(id: string): Promise<void> {
+	try {
+		const { deletedCount } = await NoteModel.deleteOne({ _id: id });
+
+		if (deletedCount === 0)
+			throw new createHttpError.NotFound(
+				`Note with id ${id} does not exist`
+			);
+
+		return;
+	} catch (error) {
+		handleMongooseExceptions(error, 'Note');
+	}
+}
--- libs/data-model/entities/src/lib/Note.entity.ts
+++ libs/data-model/entities/src/lib/Note.entity.ts
@@ -1,10 +1,21 @@
 import { SharedUsers } from '@blackstone-challenge/data-model/interfaces';
+import { Exclude, Expose } from 'class-transformer';
 
 export class Note {
+	@Exclude()
+	id = '';
+
+	@Expose()
 	title = '';
+
+	@Expose()
 	content = '';
+
 	author = '';
+
+	@Expose()
 	sharedUsers: SharedUsers[] = [];
+
 	createdAt?: Date;
 	updatedAt?: Date;
 }
