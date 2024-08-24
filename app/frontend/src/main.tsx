d6efd771db923a9cca87f5e48149a3ae9cd04c30
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-23 18:36:43 -0700

    Add the react frontend to the project

---
+++ app/frontend/src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './styles.css';
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+root.render(<StrictMode></StrictMode>);
