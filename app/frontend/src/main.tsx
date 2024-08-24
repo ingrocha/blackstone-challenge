5882b2af565c9beefeee04f8a9cf809af6bd3936
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-24 10:31:08 -0700

    Add Material Ui

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './styles.css';
+import { NotesApp } from './notesApp';
+import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
-root.render(<StrictMode></StrictMode>);
+root.render(
+	<StrictMode>
+		<BrowserRouter>
+			<NotesApp />
+		</BrowserRouter>
+	</StrictMode>
+);
