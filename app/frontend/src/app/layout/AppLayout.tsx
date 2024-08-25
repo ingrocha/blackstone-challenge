09162e48e8f7eff691e686793c9d2865a2675ecd
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-24 21:57:39 -0700

    Create skeleton Menu

--- app/frontend/src/app/layout/AppLayout.tsx
+++ app/frontend/src/app/layout/AppLayout.tsx
@@ -1,12 +1,18 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import { ReactNode } from 'react';
+import { NavBar, SideBar } from '../shared/components';
+
+const drawerWidth = 240;
 
 export const AppLayout = ({ children }: { children: ReactNode }) => {
 	return (
 		<Box sx={{ display: 'flex' }}>
+			<NavBar drawerWidth={drawerWidth}></NavBar>
+			<SideBar drawerWidth={drawerWidth}></SideBar>
 			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
 				{children}
 			</Box>
+			<Toolbar />
 		</Box>
 	);
 };
