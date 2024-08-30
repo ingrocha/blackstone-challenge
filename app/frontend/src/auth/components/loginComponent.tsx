60e91314c7c0fb2a297210490c9587f925eda788
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-30 14:32:26 -0700

    Add some error message to the form

--- app/frontend/src/auth/components/loginComponent.tsx
+++ app/frontend/src/auth/components/loginComponent.tsx
@@ -1,18 +1,51 @@
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/authLayout';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { useAppDispatch } from '../../store';
+import { toast } from 'sonner';
+import { loginUserThunk } from '../features';
+
+const schema = z.object({
+	username: z.string().min(1, { message: 'Username is required' }),
+	password: z.string().min(1, { message: 'Password is required' }),
+});
+
+export type LoginFormFields = z.infer<typeof schema>;
 
 export const LoginComponent = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<LoginFormFields>({
+		resolver: zodResolver(schema),
+	});
+
+	// const authStatus = useAppSelector((state) => state.authSlice.status);
+	const dispatch = useAppDispatch();
+
+	const onSubmit: SubmitHandler<LoginFormFields> = async (data) => {
+		const response = await dispatch(loginUserThunk(data));
+		if (response.payload) toast.success('Login successful');
+	};
+
 	return (
 		<AuthLayout title="Login">
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<Grid container>
 					<Grid item xs={12}>
 						<TextField
-							label="User"
+							label="Username"
 							type="text"
 							placeholder="Enter your username"
+							{...register('username')}
 							fullWidth
+							error={!!errors.username}
+							helperText={errors.username?.message}
 						></TextField>
 					</Grid>
 					<Grid item xs={12} sx={{ mt: 2 }}>
@@ -20,13 +53,16 @@ export const LoginComponent = () => {
 							label="Password"
 							type="password"
 							placeholder="Enter your password"
+							{...register('password')}
+							error={!!errors.password}
+							helperText={errors.password?.message}
 							fullWidth
 						></TextField>
 					</Grid>
 					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
 						<Grid item xs={12}>
-							<Button variant="contained" fullWidth>
-								Login
+							<Button type="submit" variant="contained" fullWidth>
+								{isSubmitting ? 'Logging in...' : 'Login'}
 							</Button>
 						</Grid>
 					</Grid>
